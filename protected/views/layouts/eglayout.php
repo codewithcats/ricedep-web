5f4a0194e1fa954c6dc3f7471d7f7eb709fe711d
Ratthasorn Thassana <ohmzuu@gmail.com>
2012-12-15 23:47:17 +0700

    init

---
+++ protected/views/layouts/eglayout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>Rice Department of Thialand</title>
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/egstyle.css">
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<a class="brand" href="<?php echo Yii::app()->request->baseUrl; ?>">Rice Department of Thialand</a>
+			<ul class="nav">
+			</ul>
+		</div>
+	</div>
+	<?php echo $content; ?>
+</body>
+</html>
---
+++ protected/views/site/index.php
@@ -0,0 +1,16 @@
+<div class="login-form-outer">
+	<div class="login-form hero-unit">
+		<form>
+			<fieldset>
+				<legend>Sign In</legend>
+				<label>Username</label>
+				<input type="text">
+				<label>Password</label>
+				<input type="password">
+				<div>
+					<button type="submit" class="btn btn-primary"><i class="icon icon-white icon-lock"></i>&nbsp;Sign In</button>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+</div>
