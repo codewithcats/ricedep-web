792e8c8675d5fbaf69d4964ba99d85848979ffea
Ratthasorn Thassana <ohmzuu@gmail.com>
2012-12-16 00:00:52 +0700

    go to data page after login

---
+++ protected/views/site/data.php
@@ -0,0 +1,1 @@
+<div>This is data page</div>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,12 +1,12 @@
 <div class="login-form-outer">
 	<div class="login-form hero-unit">
-		<form>
+		<form action="<?php echo Yii::app()->request->baseUrl; ?>/?r=site/login" method="post">
 			<fieldset>
 				<legend>Sign In</legend>
 				<label>Username</label>
-				<input type="text">
+				<input type="text" name="LoginForm[username]">
 				<label>Password</label>
-				<input type="password">
+				<input type="password" name="LoginForm[password]">
 				<div>
 					<button type="submit" class="btn btn-primary"><i class="icon icon-white icon-lock"></i>&nbsp;Sign In</button>
 				</div>
