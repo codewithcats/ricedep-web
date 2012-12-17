edb0ebc113eaf20adf9dd48a421b2c080928eac0
Tanawat Tassana <amp.tanawat@gmail.com>
2012-12-18 03:59:54 +0700

    Display summary report for 2554-2555 budget year

--- protected/views/resourceUsage/energyFiscalYear.php
+++ protected/views/resourceUsage/energyFiscalYear.php
@@ -1,5 +1,17 @@
+<?php
+	$indexValue; $uKey; $pKey;
+	if($index === 'elec')
+	{
+		$indexValue = 'ไฟฟ้า';
+		$uKey = 'sum_uelec_';
+		$pKey = 'sum_pelec_';
+	}
+	else if($index === 'water') $indexValue = 'น้ำ';
+	else $indexValue = 'น้ำมัน';
+
+?>
 <div class="container">
-	<h3>ข้อมูลการใช้พลังงานเปรียบเทียบปีงบประมาณ</h3>
+	<h3>ข้อมูลการใช้<?php echo $indexValue; ?>เปรียบเทียบปีงบประมาณ</h3>
 	<table class="table table-bordered">
 		<thead>
 			<tr>
@@ -7,8 +19,7 @@
 				<th rowspan="2">หน่วยงาน</th>
 				<th colspan="2" style="text-align:center;">ประจำปี 2554</th>
 				<th colspan="2" style="text-align:center;">ประจำปี 2555</th>
-				<th colspan="2" style="text-align:center;">สรุปการใช้</th>
-				<th rowspan="2">เปอร์เซนเพิ่มลด</th>
+				<th colspan="4" style="text-align:center;">สรุปการใช้</th>
 			</tr>
 			<tr>
 				<th>จำนวนหน่วย</th>
@@ -16,14 +27,32 @@
 				<th>จำนวนหน่วย</th>
 				<th>จำนวนบาท</th>
 				<th>หน่วย เพิ่ม-ลด</th>
+				<th class="center">%</th>
 				<th>บาท เพิ่ม-ลด</th>
+				<th class="center">%</th>
 			</tr>
 		</thead>
 		<tbody>
-			<?php $i=1; foreach ($names as $n): ?>
+			<?php $i=1; foreach ($rows as $r): ?>
 			<tr>
 				<td><?php echo $i ?></td>
-				<td><?php echo $n->NameIns; ?></td>
+				<td><?php echo $r['NameIns']; ?></td>
+				<td class="center"><?php echo $r[$uKey.'2554']==null? '-': $r[$uKey.'2554']; ?></td>
+				<td class="center"><?php echo $r[$pKey.'2554']==null? '-': $r[$pKey.'2554']; ?></td>
+				<td class="center"><?php echo $r[$uKey.'2555']==null? '-': $r[$uKey.'2555']; ?></td>
+				<td class="center"><?php echo $r[$pKey.'2555']==null? '-': $r[$pKey.'2555']; ?></td>
+				<?php
+					$uInc = ($r[$uKey.'2555']==null || $r[$uKey.'2554']==null)? '-': $r[$uKey.'2555'] - $r[$uKey.'2554'];
+					$uIncPercent = $uInc==='-'? '-': round($uInc/$r[$uKey.'2554']*100, 2).'%';
+				?>
+				<td class="center"><?php echo $uInc; ?></td>
+				<td class="center"><?php echo $uIncPercent; ?></td>
+				<?php
+					$pInc = ($r[$pKey.'2555']==null || $r[$pKey.'2554']==null)? '-': $r[$pKey.'2555'] - $r[$pKey.'2554'];
+					$pIncPercent = $pInc==='-'? '-': round($pInc/$r[$pKey.'2554']*100, 2).'%';
+				?>
+				<td class="center"><?php echo $pInc; ?></td>
+				<td class="center"><?php echo $pIncPercent; ?></td>
 			</tr>
 			<?php $i++;endforeach; ?>
 		</tbody>
